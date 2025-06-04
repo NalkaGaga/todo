7dd033ffe9f1c2ea0e2c904900210dda84fa15ec
Nimo Abukar <hooyo352063@gmail.com>
2025-06-04 21:41:46 +0300

    nimo nimo setted

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
-# Create your views here.
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'todoapp/task_list.html', {'tasks': tasks})
+
+def add_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todoapp:task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'todoapp/add_task.html', {'form': form})
+
+def complete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.completed = True
+    task.save()
+    return redirect('todoapp:task_list')
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('todoapp:task_list')
