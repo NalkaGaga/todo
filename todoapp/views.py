2f58dbbf56b3246a233640da055ef101829c965b
Gaga-Marda-Bur <gagabourbour@gmail.com>
2025-06-05 13:19:38 +0300

    added task_detail page and updated all pages

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import Task
 from .forms import TaskForm
 
 def task_list(request):
     tasks = Task.objects.all()
     return render(request, 'todoapp/task_list.html', {'tasks': tasks})
+
+def task_detail(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    return render(request, 'todoapp/task_detail.html', {'task': task})
 
 def add_task(request):
     if request.method == 'POST':
